338843c875d77de45544e5901c4709a5339c2b92
ManukZatikyan <zatikyan.manuk@mail.ru>
2023-04-20 17:01:42 +0400

    my last commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 function App() {
-  const [data, setData] = useState('');
-
-  useEffect(() => {
-    (async function () {
-      const { text } = await (await fetch(`/api/message`)).json();
-      setData(text);
-    })();
-  });
-
-  return <div>kak dela?</div>;
+  return <div> kak dela?</div>;
 }
 
 export default App;
